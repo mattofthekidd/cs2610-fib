8d3df45716f65a1ae3329d73b4dcd3311a15ac5d
Matthew Kidd <mattofthekidd@gmail.com>
2017-03-12 12:15:40 -0600

    finished product

---
+++ scratchpad.js
@@ -0,0 +1,248 @@
+/*
+ * This is a JavaScript Scratchpad.
+ *
+ * Enter some JavaScript, then Right Click or choose from the Execute Menu:
+ * 1. Run to evaluate the selected text (Cmd-R),
+ * 2. Inspect to bring up an Object Inspector on the result (Cmd-I), or,
+ * 3. Display to insert the result in a comment after the selection. (Cmd-L)
+ */
+
+function createDiv() {
+	var div = document.createElement('div');
+	div.setAttribute("class", " stuff-box shadowed");
+	document.body.appendChild(div);
+	return div;
+}
+function addLink(node) {
+	var value = "";
+	var p = document.createElement('p');
+	var link = document.createElement('a');
+	if(node.id.toString() == "fib") {
+		value = ("A000045");
+		p.textContent = ("Fibonacci");
+	}
+	else if(node.id.toString() == "pell") {
+		value = ("A000129");
+		p.textContent = ("Pell");
+	}
+	else {
+		value = ("A000073");
+		p.textContent = ("Tribonacci");
+	}
+	link.href = "https://oeis.org/" + value;
+	link.appendChild(p);
+
+
+	node.appendChild(link);
+
+}
+(function () {
+	document.querySelector('title');
+	document.title = "JavaScript Recursive Sequences";
+})();
+
+function fibHelper(n) {
+	var value;
+	var div = document.createElement('div');
+	if (n < 2) {
+		if (n === 0) {
+			value = 0;
+		}
+		else if (n === 1) {
+			value = 1;
+		}
+		var p = document.createElement('p');
+		p.textContent = 'f(' + n + ') = ' + value;
+		div.appendChild(p);
+	}
+	else {
+		var left = fibHelper(n - 1);
+		var fib_class = left.html.getAttribute("class");
+		left.html.setAttribute("class", fib_class + " fib-left");
+
+		var right = fibHelper(n - 2);
+		fib_class = right.html.getAttribute("class");
+		right.html.setAttribute("class", fib_class + " fib-right");
+
+		value = left.value + right.value;
+		var p = document.createElement('p');
+		p.textContent = 'f(' + n + ') = ' + value;
+		div.appendChild(p);
+
+		div.appendChild(left.html);
+		div.appendChild(right.html);
+	}
+	return {'value': value, 'html': div};
+}
+var fib = function (n, node) {
+	node.setAttribute("id", "fib");
+	addLink(node);
+	var tree = fibHelper(n);
+	node.appendChild(tree.html);
+};
+
+function pellHelper(n) {
+	var value;
+	var div = document.createElement('div');
+	if(n < 2) {
+		if (n === 0) {
+			value = 0;
+		}
+		else if (n === 1) {
+			value = 1;
+		}
+		var p = document.createElement('p');
+		p.textContent = 'p(' + n + ') = ' + value;
+		div.appendChild(p);
+	}
+	else {
+		var left = pellHelper(n - 1);
+		var pell_class = left.html.getAttribute("class");
+		left.html.setAttribute("class", pell_class + " fib-left");
+
+		var right = pellHelper(n - 2);
+		pell_class = right.html.getAttribute("class");
+		right.html.setAttribute("class", pell_class + " fib-right");
+
+		value = 2*left.value + 2*right.value;
+		var p = document.createElement('p');
+		p.textContent = 'p(' + n + ') = ' + value;
+		div.appendChild(p);
+
+		div.appendChild(left.html);
+		div.appendChild(right.html);
+	}
+	return{'value': value, 'html': div};
+};
+var pell = function(n, node) {
+	node.setAttribute("id", "pell");
+	addLink(node);
+	var tree = pellHelper(n);
+	node.appendChild(tree.html);
+};
+
+function tribHelper(n) {
+	var value;
+	var div = document.createElement('div');
+	if(n < 3) {
+		if (n === 0 || n === 1) {
+			value = 0;
+		}
+		else if (n === 2) {
+			value = 1;
+		}
+		var p = document.createElement('p');
+		p.textContent = 't(' + n + ') = ' + value;
+		div.appendChild(p);
+	}
+	else {
+		var left = tribHelper(n - 1);
+		var trib_class = left.html.getAttribute("class");
+		left.html.setAttribute("class", trib_class + " fib-left");
+
+		var middle = tribHelper(n - 2);
+		trib_class = middle.html.getAttribute("class");
+		middle.html.setAttribute("class", trib_class + " fib-middle");
+
+		var right = tribHelper(n - 2);
+		trib_class = right.html.getAttribute("class");
+		right.html.setAttribute("class", trib_class + " fib-right");
+
+		value = left.value + middle.value + right.value;
+		var p = document.createElement('p');
+		p.textContent = 't(' + n + ') = ' + value;
+		div.appendChild(p);
+
+		div.appendChild(left.html);
+		div.appendChild(middle.html);
+		div.appendChild(right.html);
+	}
+	return{'value': value, 'html': div};
+}
+var trib = function(n, node) {
+	node.setAttribute("id", "trib");
+	addLink(node);
+	var tree = tribHelper(n);
+	node.appendChild(tree.html);
+};
+
+
+
+(function() {
+	fib(11, createDiv());
+	pell(11, createDiv());
+	trib(11, createDiv());
+})();
+
+(function() {
+	var style = document.createElement('style');
+	style.textContent =
+		"a:hover {" +
+		"    color: red;" +
+		"}" +
+		"a {" +
+		"    text-align: left;" +
+		"}" +
+		"#fib {" +
+		"    background: lightseagreen;" +
+		"    display: inline-block;" +
+		"    width: 9470px;" +
+		"}" +
+		"" +
+		"#pell {" +
+		"    background: lightseagreen;" +
+		"    display: inline-block;" +
+		"    width: 9650px;" +
+		"}" +
+		"" +
+		"#trib {" +
+		"    background: lightseagreen;" +
+		"    display: inline-block;" +
+		"    width: 44920px;" +
+		"}" +
+		".shadowed {" +
+		"    text-shadow: 1px 1px 2px black;" +
+		"    color:       white;" +
+		"}" +
+		".stuff-box {" +
+		"    font-family: 'helvetica neue', helvetica, sans-serif;" +
+		"    letter-spacing: 1px;" +
+		"    text-transform: capitalize;" +
+		"    text-align: center;" +
+		"    padding: 3px 10px;" +
+		"    margin: 10px;" +
+		"    cursor: pointer;" +
+		"    border-color: lightskyblue;" +
+		"    border-radius: 10px;" +
+		"    border-width: 2px;" +
+		"    border-style: solid;" +
+		"}" +
+		".fib-left {" +
+		"    float: left;" +
+		"    display: inline-block;" +
+		"    margin-right: 2px;" +
+		"    background: rgba(125, 1, 249, .1);" +
+		"}" +
+		"" +
+		".fib-right {" +
+		"    float: right;" +
+		"    display: inline-block;" +
+		"    margin-left: 2px;" +
+		"    background: rgba(125, 1, 249, .1);" +
+		"}" +
+		"" +
+		".fib-middle {" +
+		"    display: inline-block;" +
+		"    margin: 0px 2px 0px 2px;" +
+		"    background: rgba(125, 1, 249, .1);" +
+		"}" +
+		"" +
+		".fib-left span {" +
+		"    mix-blend-mode: hard-light;" +
+		"}" +
+		"" +
+		".fib-right span {" +
+		"    mix-blend-mode: hard-light;" +
+		"}";
+	document.querySelector('body').appendChild(style);
+})();
